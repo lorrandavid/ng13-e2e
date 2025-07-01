4541f93677ac9e5b0bf78a74560a1020f1293a4e
Lorran David <lorrantd1@gmail.com>
2025-07-01 00:09:48 -0300

    add other reporter temporaly

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,4 +1,5 @@
-import { expect, test } from '@bgotink/playwright-coverage';
+// import { expect, test } from '@bgotink/playwright-coverage';
+import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
